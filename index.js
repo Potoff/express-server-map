d455f655fc17810c46674af96129aaff3ba19488
Potoff <services.ankaamusic@gmail.com>
2023-11-07 17:15:26 +0100

    add router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
+
+app.use('/api/search/', require('./routes/searchResults'));
 
 app.listen(PORT, () => {
   console.log('Le serveur Express est en cours d\'exécution sur le port ' +  PORT);
---
+++ routes/searchResults.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const url = require('url');
+const router = express.Router(); 
+
+router.get('/:query', async (req, res) => {
+    try{
+        // add api key and query strings
+        const params = new URLSearchParams({ 
+          access_token: process.env.API_KEY,
+          ...url.parse(req.url, true).query,
+        })
+        const query = req.params.query;
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`);
+        res.status(200).json(data);
+      }
+      catch(err) {
+        res.status(500).json({ message: err.message });
+      }
+    });
+
+module.exports = router;
